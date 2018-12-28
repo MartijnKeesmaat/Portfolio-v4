0bfb098972d7a1806f62e8395757ce4db2275fe8
Martijn Keesmaat <martijnpk@ziggo.nl>
2018-12-28 23:50:44 +0100

    start of random chars

--- app/scripts/slider.js
+++ app/scripts/slider.js
@@ -1,28 +1,35 @@
-window.onload = showSlider
+// Animate in
+// window.onload = showSlider;
 
 function showSlider() {
   TweenMax.from('.slider__body', 2, {
     y: '150%',
+    rotationX: 100,
+    scaleX: 1.3,
     ease: Expo.easeOut
-  })
+  });
 
-  TweenMax.staggerFrom('.slider-ui', 1, {
+  TweenMax.staggerFrom('.slider-ui', 0.8, {
     opacity: 0,
-    delay: .5,
+    delay: 0.6,
+    rotationX: 30,
     y: 20
-  })
+  });
 }
 
-
+// Detect scroll direction
 function detectMouseWheelDirection(e) {
   var delta = null,
     direction = false;
-  if (!e) { // if the event is not provided, we get it from the window object
+  if (!e) {
+    // if the event is not provided, we get it from the window object
     e = window.event;
   }
-  if (e.wheelDelta) { // will work in most cases
+  if (e.wheelDelta) {
+    // will work in most cases
     delta = e.wheelDelta / 60;
-  } else if (e.detail) { // fallback for Firefox
+  } else if (e.detail) {
+    // fallback for Firefox
     delta = -e.detail / 2;
   }
   if (delta !== null) {
@@ -32,71 +39,93 @@ function detectMouseWheelDirection(e) {
   return direction;
 }
 
-var scrollCounter = 0
+var scrollCounter = 0;
 
 function handleMouseWheelDirection(direction) {
-
   if (scrollCounter % 50 == 0) {
     if (direction == 'down') {
-      showNextSlide()
+      showNextSlide();
     } else if (direction == 'up') {
-      showPrevSlide()
+      showPrevSlide();
     } else {
       // this means the direction of the mouse wheel could not be determined
     }
   }
 
-  scrollCounter++
+  scrollCounter++;
 }
 
-document.onmousewheel = function (e) {
+document.onmousewheel = function(e) {
   handleMouseWheelDirection(detectMouseWheelDirection(e));
 };
 if (window.addEventListener) {
-  document.addEventListener('DOMMouseScroll', function (e) {
+  document.addEventListener('DOMMouseScroll', function(e) {
     handleMouseWheelDirection(detectMouseWheelDirection(e));
   });
 }
 
+// Set DOM
+const SLIDER_DOM = {
+  images: document.querySelectorAll('.slider__img'),
+  titles: document.querySelectorAll('.slider__title'),
+  displayTitle: document.querySelector('.slider-display-title')
+};
+
+// Set vars
 let activeSlideCount = 0;
 let nextSlide = activeSlideCount + 1;
-const SLIDER_IMAGES = document.querySelectorAll('.slider__img');
-const SLIDER_TITLES = document.querySelectorAll('.slider__title');
-let SLIDER_TITLE = document.querySelector('.slider-display-title');
-let activeSlide = SLIDER_IMAGES[activeSlideCount];
+let activeSlide = SLIDER_DOM.images[activeSlideCount];
+const CHARACTERS = 'abcdefghijklmnopqrstuvwxyz#%&^+=-';
 
-const hideImages = () => SLIDER_IMAGES.forEach(i => i.style.opacity = 0);
-const showCurrentImage = () => SLIDER_IMAGES[activeSlideCount].style.opacity = 1;
-const checkIfSlidesEnded = () => {
-  if (SLIDER_IMAGES.length == activeSlideCount) activeSlideCount = 0
-}
-const checkIfSlidesPreceeded = () => {
-  if (activeSlideCount === -1) activeSlideCount = SLIDER_IMAGES.length - 1
+// Fns
+const hideImages = () => SLIDER_DOM.images.forEach(i => (i.style.opacity = 0));
+
+function showCurrentImage() {
+  SLIDER_DOM.images[activeSlideCount].style.opacity = 1;
 }
 
-const showCurrentText = () => {
-  SLIDER_TITLE.innerText = SLIDER_TITLES[activeSlideCount].innerText
+function checkIfSlidesEnded() {
+  if (SLIDER_DOM.images.length == activeSlideCount) activeSlideCount = 0;
 }
 
+function checkIfSlidesPreceeded() {
+  if (activeSlideCount === -1) activeSlideCount = SLIDER_DOM.images.length - 1;
+}
+
+function showCurrentTitle() {
+  var currentTitle = SLIDER_DOM.displayTitle;
+  var nextTitle = SLIDER_DOM.titles[activeSlideCount].innerText;
+
+  var r = Math.floor(Math.random() * CHARACTERS.length);
+  var randomChar = CHARACTERS[r];
+  currentTitle.innerText = randomChar;
+}
+
+// Init
 hideImages();
 showCurrentImage();
 
+// Control slides
 function showNextSlide() {
-  activeSlideCount++
+  activeSlideCount++;
   checkIfSlidesEnded();
 
   hideImages();
   showCurrentImage();
 
-  showCurrentText();
+  showCurrentTitle();
+
+  clearInterval(slideTimer);
+  slideTimer = setInterval(showNextSlide, 7000);
 }
 
 function showPrevSlide() {
-  activeSlideCount--
+  activeSlideCount--;
   checkIfSlidesPreceeded();
   hideImages();
   showCurrentImage();
-  showCurrentText();
+  showCurrentTitle();
 }
 
-setInterval(showNextSlide, 7000);
+// Autoplay
+let slideTimer = setInterval(showNextSlide, 7000);
