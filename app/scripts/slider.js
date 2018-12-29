f00b0ff8bcec105e5ee78ce2006de1d62edeec64
Martijn Keesmaat <martijnpk@ziggo.nl>
2018-12-29 01:23:07 +0100

    Project slider animated

--- app/scripts/slider.js
+++ app/scripts/slider.js
@@ -1,5 +1,5 @@
 // Animate in
-// window.onload = showSlider;
+window.onload = showSlider;
 
 function showSlider() {
   TweenMax.from('.slider__body', 2, {
@@ -55,11 +55,11 @@ function handleMouseWheelDirection(direction) {
   scrollCounter++;
 }
 
-document.onmousewheel = function(e) {
+document.onmousewheel = function (e) {
   handleMouseWheelDirection(detectMouseWheelDirection(e));
 };
 if (window.addEventListener) {
-  document.addEventListener('DOMMouseScroll', function(e) {
+  document.addEventListener('DOMMouseScroll', function (e) {
     handleMouseWheelDirection(detectMouseWheelDirection(e));
   });
 }
@@ -68,7 +68,8 @@ if (window.addEventListener) {
 const SLIDER_DOM = {
   images: document.querySelectorAll('.slider__img'),
   titles: document.querySelectorAll('.slider__title'),
-  displayTitle: document.querySelector('.slider-display-title')
+  displayTitle: document.querySelector('.slider-display-title'),
+  desc: document.querySelectorAll('.slider__desc')
 };
 
 // Set vars
@@ -78,11 +79,42 @@ let activeSlide = SLIDER_DOM.images[activeSlideCount];
 const CHARACTERS = 'abcdefghijklmnopqrstuvwxyz#%&^+=-';
 
 // Fns
-const hideImages = () => SLIDER_DOM.images.forEach(i => (i.style.opacity = 0));
+const hideImages = () => SLIDER_DOM.images.forEach(i => {
+  (
+    TweenMax.to(i, 0, {
+      opacity: 0,
+      y: 0,
+      rotationX: 0,
+      scale: 1
+    })
+  )
+});
 
 function showCurrentImage() {
-  SLIDER_DOM.images[activeSlideCount].style.opacity = 1;
+  TweenMax.to(SLIDER_DOM.images[activeSlideCount], .2, {
+    y: 30,
+    opacity: 1,
+    rotationX: 100,
+    scale: 1.1
+  })
 }
+
+function showCurrentDesc() {
+  TweenMax.to(SLIDER_DOM.desc[activeSlideCount], .6, {
+    x: 0,
+    opacity: 1,
+    rotationY: 0
+  })
+}
+
+const hideCurrentDesc = () => SLIDER_DOM.desc.forEach(i => {
+  (
+    TweenMax.to(i, 0, {
+      x: -100,
+      opacity: 0,
+    })
+  )
+});
 
 function checkIfSlidesEnded() {
   if (SLIDER_DOM.images.length == activeSlideCount) activeSlideCount = 0;
@@ -96,14 +128,16 @@ function showCurrentTitle() {
   var currentTitle = SLIDER_DOM.displayTitle;
   var nextTitle = SLIDER_DOM.titles[activeSlideCount].innerText;
 
-  var r = Math.floor(Math.random() * CHARACTERS.length);
-  var randomChar = CHARACTERS[r];
-  currentTitle.innerText = randomChar;
+  // var r = Math.floor(Math.random() * CHARACTERS.length);
+  // var randomChar = CHARACTERS[r];
+  currentTitle.innerText = nextTitle;
 }
 
 // Init
 hideImages();
 showCurrentImage();
+showCurrentDesc();
+hideCurrentDesc();
 
 // Control slides
 function showNextSlide() {
@@ -112,8 +146,9 @@ function showNextSlide() {
 
   hideImages();
   showCurrentImage();
-
+  showCurrentDesc()
   showCurrentTitle();
+  hideCurrentDesc();
 
   clearInterval(slideTimer);
   slideTimer = setInterval(showNextSlide, 7000);
@@ -123,8 +158,10 @@ function showPrevSlide() {
   activeSlideCount--;
   checkIfSlidesPreceeded();
   hideImages();
+  showCurrentDesc();
   showCurrentImage();
   showCurrentTitle();
+  hideCurrentDesc();
 }
 
 // Autoplay
